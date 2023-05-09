cea9be83b9f4f7c3b74eaed491cfad7b9bdfaa0a
EmpiricalCode <daniellongschool@gmail.com>
2023-05-09 00:00:57 -0400

    Set up constructor for sphere entity

--- Core/Entities/Sphere.java
+++ Core/Entities/Sphere.java
@@ -1,5 +1,17 @@
 package Core.Entities;
 
-public class Sphere {
+import Core.Structures.Entity;
+import Core.Utility.Vector3D;
+
+public class Sphere extends Entity {
     
+    private double radius;
+
+    // Main constructor
+    public Sphere(Vector3D position, double radius) {
+
+        super(position);
+
+        this.radius = radius;
+    }
 }
