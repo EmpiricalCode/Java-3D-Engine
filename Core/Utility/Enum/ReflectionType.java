907fb9395fda1c2d547e3ff9f7ec646818d7af04
EmpiricalCode <daniel.long@student.tdsb.on.ca>
2023-05-12 11:04:33 -0400

    Removed unnecessary code in ReflectionType enum

--- Core/Utility/Enum/ReflectionType.java
+++ Core/Utility/Enum/ReflectionType.java
@@ -1,15 +1,6 @@
 package Core.Utility.Enum;
 
 public enum ReflectionType {
-
-    SPECULAR (0),
-    DIFFUSE (1);
-
-    // 0 = diffuse reflection
-    // 1 = specular reflection
-    private final int reflectionId;
-
-    ReflectionType (int reflectionId) {
-        this.reflectionId = reflectionId;
-    }
+    SPECULAR,
+    DIFFUSE;
 }
