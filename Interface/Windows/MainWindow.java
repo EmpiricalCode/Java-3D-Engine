03aea91a96fd2313c18ee1175b756a9aba9a9fbc
EmpiricalCode <daniel.long@student.tdsb.on.ca>
2023-05-12 10:59:55 -0400

    Removed unnecessary comments

--- Interface/Windows/MainWindow.java
+++ Interface/Windows/MainWindow.java
@@ -28,16 +28,6 @@ public class MainWindow extends Window {
         environment.addEntity(new Sphere(new Vector3D(10, 0, -205), new ColorRGB(0, 0, 0), ReflectionType.SPECULAR, 200));
         
         RenderWindow renderWindow = new RenderWindow(environment, 9);
-
-        // Vector3D original = new Vector3D(0, 0, 5);
-        // Vector3D originalDir = new Vector3D(15, 0, 0);
-
-        // while (true) {
-
-        //     original = Vector3D.add(original, new Vector3D(0, 0.3, 0.1));
-        //     // originalDir = Vector3D.add(originalDir, new Vector3D(0, 0.5, 0));
-        //     environment.setCamera(new Camera(original, originalDir));
-            renderWindow.render();
-        // }
+        renderWindow.render();
     }
 }
