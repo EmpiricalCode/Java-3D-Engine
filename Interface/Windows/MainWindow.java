e6c5543162de0bce14efc382325e1bddcff4ed85
EmpiricalCode <daniellongschool@gmail.com>
2023-06-07 09:38:43 -0400

    Saving work

--- Interface/Windows/MainWindow.java
+++ Interface/Windows/MainWindow.java
@@ -100,7 +100,7 @@ public class MainWindow extends Window {
         Environment environment = new Environment(new Camera(new Vector3D(-1, -10, 10), new Vector3D(15, 0, 0)));
 
         environment.addEntity(new RectangularPrism(new Vector3D(15, 15, 0), new ColorRGB(255, 255, 255), 0.2, ReflectionType.SPECULAR, 20, 10, 20));
-        environment.addEntity(new RectangularPrism(new Vector3D(15, -10, 0), new ColorRGB(255, 100, 255), 0.2, ReflectionType.DIFFUSE, 20, 10, 15));
+        environment.addEntity(new RectangularPrism(new Vector3D(15, -10, 0), new ColorRGB(255, 100, 255), 0.2, ReflectionType.SPECULAR, 20, 10, 15));
         environment.addEntity(new RectangularPrism(new Vector3D(15, 5, 0), new ColorRGB(255, 255, 255), 0.5, ReflectionType.SPECULAR, 5, 5, 5));
         environment.addEntity(new RectangularPrism(new Vector3D(15, 15, -55), new ColorRGB(255, 255, 100), 1, ReflectionType.DIFFUSE, 100, 100, 80));
         // environment.addEntity(new Sphere(new Vector3D(10, 0, 0), new ColorRGB(200, 100, 255), 1, ReflectionType.DIFFUSE, 4));
